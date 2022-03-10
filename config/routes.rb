bdd8645f813c8624bb5177defe45a5fb90b64d44
Marlena Tsang <Marlena345@gmail.com>
2022-03-09 20:10:25 -0500

    commit tree f2a13be8cfa6f46edcf909485150b03533923ccd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'signups/:create'
   resources :activities, only: [:index, :destroy]
   resources :campers, only: [:index, :show, :create]
-  resources :signups, only: [:create]
+  resources :signups, only: :create
 end
